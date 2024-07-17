81b2609806ccde59350e92b1f00e7313ca9c9948
ezomo <danielohiozoje@icloud.com>
2024-07-17 15:06:14 +0900

    pub

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 use std::collections::HashMap;
 
-
 pub struct DtwcSetting {
-    l_id: usize,
-    r_id: usize,
-    robot_center_to_wheel_distance:f64
+    pub l_id: usize,
+    pub r_id: usize,
+    pub robot_center_to_wheel_distance: f64,
 }
 
 impl DtwcSetting {
-    pub fn move_chassis(&self, linear_x: f64, linear_y: f64, angular_z: f64) -> HashMap<usize, f64> {
+    pub fn move_chassis(
+        &self,
+        linear_x: f64,
+        linear_y: f64,
+        angular_z: f64,
+    ) -> HashMap<usize, f64> {
         let [_xrpm, _yrpm, _yaw] = [-linear_x, linear_y, angular_z];
         //回転成分　θ・R _yawの０基準を前方にしてロボットからの距離をかける
         let mut rotation_component: f64 = _yaw * 0.04 * self.robot_center_to_wheel_distance;

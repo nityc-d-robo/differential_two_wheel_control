0f7f4bb0c7a391e3e8e557c11259940bb43d78e7
ezomo <danielohiozoje@icloud.com>
2024-07-17 14:58:03 +0900

    robot_center_to_wheel_distanceの変更可能

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 use std::collections::HashMap;
 
-const ROBOT_CENTER_TO_WHEEL_DISTANCE: f64 = 0.37; //ロボットの中心からホイールまでの距離[m]
 
 pub struct DtwcSetting {
     l_id: usize,
     r_id: usize,
+    robot_center_to_wheel_distance:f64
 }
 
 impl DtwcSetting {
-    pub fn move_chassis(self, linear_x: f64, linear_y: f64, angular_z: f64) -> HashMap<usize, f64> {
+    pub fn move_chassis(&self, linear_x: f64, linear_y: f64, angular_z: f64) -> HashMap<usize, f64> {
         let [_xrpm, _yrpm, _yaw] = [-linear_x, linear_y, angular_z];
         //回転成分　θ・R _yawの０基準を前方にしてロボットからの距離をかける
-        let mut rotation_component: f64 = _yaw * 0.04 * ROBOT_CENTER_TO_WHEEL_DISTANCE;
+        let mut rotation_component: f64 = _yaw * 0.04 * self.robot_center_to_wheel_distance;
 
         if _xrpm < 10.0 {
             rotation_component *= 1.5;

fd7e14b3e7fa08580f95cbeabb459d6501ea9796
ezomo <danielohiozoje@icloud.com>
2024-08-30 21:58:42 +0900

    再作成

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,48 @@
 use std::collections::HashMap;
 
+pub struct Tire {
+    pub id: usize,
+    pub raito: f64,
+}
+
+pub struct Chassis {
+    pub l: Tire,
+    pub r: Tire,
+}
 pub struct DtwcSetting {
-    pub l_id: usize,
-    pub r_id: usize,
+    pub chassis: Chassis,
     pub robot_center_to_wheel_distance: f64,
+    pub max_pawer_input: f64,
+    pub max_pawer_output: f64,
+    pub max_revolution: f64,
 }
 
 impl DtwcSetting {
-    pub fn move_chassis(
-        &self,
-        linear_x: f64,
-        linear_y: f64,
-        angular_z: f64,
-    ) -> HashMap<usize, f64> {
-        let [_xrpm, _yrpm, _yaw] = [-linear_x, linear_y, angular_z];
-        //回転成分　θ・R _yawの０基準を前方にしてロボットからの距離をかける
-        let mut rotation_component: f64 = _yaw * 0.04 * self.robot_center_to_wheel_distance;
+    pub fn move_chassis(&self,linear_x: f64,linear_y: f64,angular_z: f64,) -> HashMap<usize, f64> {
+        let power: f64 = (linear_x.powf(2.) + linear_y.powf(2.))
+            .sqrt()
+            .min(self.max_pawer_input);
+        
+        // 感
+        let revolution = (angular_z/self.max_revolution)*self.robot_center_to_wheel_distance;
+        let mut left_speed = self.chassis.r.raito * (power + revolution);
+        let mut right_speed = self.chassis.l.raito * (power - revolution);
 
-        if _xrpm < 10.0 {
-            rotation_component *= 1.5;
-        }
-        let left_speed: f64 = _yrpm - rotation_component;
-        let right_speed: f64 = _yrpm + rotation_component;
+
+        // モーターの回転方向が逆だから
+        left_speed *= -1.;
+
+        // 範囲内に止める
+        left_speed = left_speed.max(-self.max_pawer_output);
+        left_speed = left_speed.min(self.max_pawer_output);
+
+        right_speed = right_speed.max(-self.max_pawer_output);
+        right_speed = right_speed.min(self.max_pawer_output);
+
 
         let mut re_motor_powar = HashMap::new();
-        re_motor_powar.insert(self.l_id, -1. *left_speed * 3.5);
-        re_motor_powar.insert(self.r_id,  right_speed * 3.5);
+        re_motor_powar.insert(self.chassis.l.id,left_speed);
+        re_motor_powar.insert(self.chassis.r.id, right_speed);
 
         re_motor_powar
     }
